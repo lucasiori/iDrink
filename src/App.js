8e849c61537f9d3fc900d2774f358ca73548ea4e
Lucas Iori <liori@aktienow.com>
2020-06-06 10:05:44 -0300

    Creation of routes and application global styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import Routes from './routes';
+
+import GlobalStyle from './styles/global';
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <GlobalStyle />
+      <Routes />
+    </>
+  );
 }
 
 export default App;
